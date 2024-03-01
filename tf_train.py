9dd065de6712a714906dce9d4f351177750971c7
Shalom Guslits <19bsg@queensu.ca>
2024-02-29 19:26:03 -0500

    porting over some things from notebook to python training file

--- tf_train.py
+++ tf_train.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+from scipy.stats import gennorm
 from preprocess.acf import *
 from preprocess.gaussianize import *
 
@@ -10,6 +11,7 @@ import tensorflow as tf
 # strategy = tf.distribute.MirroredStrategy()
 # with strategy.scope():
 
+from tensorflow import convert_to_tensor
 from tensorflow.random import normal
 from tensorflow.keras.models import load_model
 #from google.colab import files
@@ -19,7 +21,7 @@ from model.tf_tcn import *
 
 
 # Data
-file_name = "^GSPC_historical_data_2009-05-01_to_2018-12-01"
+file_name = "SP500_daily"
 file_path = "data/"+file_name+".csv"
 generator_path = ""
 
@@ -40,16 +42,24 @@ receptive_field_size = 127  # p. 17
 log_returns_rolled = rolling_window(log_returns_preprocessed, receptive_field_size)
 data_size = log_returns.shape[0]
 
+# Noise
+beta = 1.5
+use_gen_noise = False
+
+generalized_noise = convert_to_tensor(gennorm.rvs(beta=beta, size=[512, 1, data_size + receptive_field_size - 1, 3]))
+gaussian_noise = normal([512, 1, len(log_returns_preprocessed) + receptive_field_size - 1, 3])
+
 # Training
 
 train = True
 
 if train:
 	gan = GAN(discriminator, generator, 2 * receptive_field_size - 1, lr_d=1e-4, lr_g=3e-5)
-	gan.acf_real = acf(log_returns_preprocessed, 250)
-	gan.abs_acf_real = acf(log_returns_preprocessed**2, 250)
-	gan.le_real = acf(log_returns_preprocessed, 250, le=True)
-	gan.fixed_noise = normal([128, 1, data_size + receptive_field_size - 1, 3])
+	gan.acf_real = acf(log_returns_preprocessed, 250)[:-1]
+	gan.abs_acf_real = acf(log_returns_preprocessed**2, 250)[:-1]
+	gan.le_real = acf(log_returns_preprocessed, 250, le=True)[:-1]
+	gan.fixed_noise = convert_to_tensor(gennorm.rvs(beta=beta, size=[128, 1, data_size + receptive_field_size - 1, 3])) if use_gen_noise \
+		else normal([128, 1, len(log_returns_preprocessed) + receptive_field_size - 1, 3])
 	data = np.expand_dims(np.moveaxis(log_returns_rolled, 0,1), 1).astype('float32')
 	batch_size = 1000
 	n_batches = 3000
